058227db111d10b6cc11e03d4d0d07d725b48ec8
Peti <antalpeti@users.noreply.github.com>
2017-06-19 23:59:31 +0200

    Process JSON PHP examples. #99

---
+++ json/json_php/json_demo_db_post.php
@@ -0,0 +1,11 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+$obj = json_decode($_POST["x"], false);
+
+$conn = new mysqli("myServer", "myUser", "myPassword", "Northwind");
+$result = $conn->query("SELECT name FROM ".$obj->table." LIMIT ".$obj->limit);
+$outp = array();
+$outp = $result->fetch_all(MYSQLI_ASSOC);
+
+echo json_encode($outp);
+?>
